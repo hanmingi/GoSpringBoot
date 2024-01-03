31223352ea4761fee525402941037859632cb485
hanmingi <hk335078@naver.com>
2024-01-03 23:33:28 +0900

    Part 1-3 : Lombok 설치 및 log Test

--- src/main/java/com/GoSpringBoot/Starter/Ctl/StarterCtl.java
+++ src/main/java/com/GoSpringBoot/Starter/Ctl/StarterCtl.java
@@ -1,22 +1,26 @@
 package com.GoSpringBoot.Starter.Ctl;
 
 import io.swagger.v3.oas.annotations.Hidden;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
+@Slf4j
 public class StarterCtl {
 
     @GetMapping("/temp")
-    public String temp(){
+    public String temp()
+    {
         return "Give It A Go";
     }
 
     @GetMapping("/SwaggerTest")
     public String SwaggerTest()
     {
+        log.info("lombok log Test");
         return "Test Api";
     }
 
