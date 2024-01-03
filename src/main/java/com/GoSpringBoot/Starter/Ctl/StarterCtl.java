e1eb7dc74c45356007c44cb6f64f34d6fab92172
hanmingi <hk335078@naver.com>
2024-01-03 23:40:08 +0900

    Part 1-4 : @GetMapping 활용

--- src/main/java/com/GoSpringBoot/Starter/Ctl/StarterCtl.java
+++ src/main/java/com/GoSpringBoot/Starter/Ctl/StarterCtl.java
@@ -2,9 +2,7 @@ package com.GoSpringBoot.Starter.Ctl;
 
 import io.swagger.v3.oas.annotations.Hidden;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
@@ -29,5 +27,19 @@ public class StarterCtl {
     public String Hidden()
     {
         return "Hidden";
+    }
+
+    @GetMapping("/start")
+    public String getStart(@RequestParam("name") String name)
+    {
+        log.info("name => {}", name);
+        return "OK";
+    }
+
+    @GetMapping("/start2/{name}")
+    public String getStart2(@PathVariable("name") String name)
+    {
+        log.info("name ==> {}", name);
+        return "OK";
     }
 }
